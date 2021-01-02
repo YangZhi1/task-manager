1523f102022df3a2d0694ab245331ab32da9116d
YangZhi1 <lyangzhi1@gmail.com>
2021-01-02 22:52:10 +0800

    First commit

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  lists: List[];
+  tasks: Task[];
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.taskService.getTasks(params.listId).subscribe((tasks: Task[]) => {
+          this.tasks = tasks;
+        })
+      }
+    )
+
+    this.taskService.getLists().subscribe((lists: List[]) => {
+      this.lists = lists;
+    })
+  }
+
+  onTaskClick(task: Task) {
+    // We want to set the task to be completed
+    this.taskService.complete(task).subscribe(() => {
+      // change task to completed
+      task.completed = !task.completed
+    });
+  }
+
+}
